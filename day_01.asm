f4d794be949b0f262970ac77bcfa732bde52a875
andrewjhaman <andrewjhaman@gmail.com>
2023-12-02 16:35:28 -0500

    Factor out opening of a file to a function

--- day_01.asm
+++ day_01.asm
@@ -12,31 +12,50 @@ extern CreateFileA
 
 %define FILE_ATTRIBUTE_NORMAL 0x00000080
 
+%macro FUNCTION_PROLOGUE 1
+    push rbp 
+    mov rbp, rsp
+
+    sub rsp, %1
+%endmacro
+
+%macro FUNCTION_EPILOGUE 1
+    add rsp, %1
+    pop rbp
+    ret
+%endmacro
 
 section .data
-    my_filename db "my_filename.txt"
+    my_filename db "my_filename.txt", 0 
+    my_other_filename db "my_other_filename.txt", 0
 
 section .text
 
 
+open_file:
+    FUNCTION_PROLOGUE 0x30
 
-_start:
-    mov rcx, my_filename
     mov edx, (GENERIC_READ | GENERIC_WRITE)
     xor r8, r8
     xor r9, r9
     xor eax, eax
 
-    sub rsp, 0x30
     mov [rsp+0x20], dword CREATE_ALWAYS
     mov [rsp+0x28], dword FILE_ATTRIBUTE_NORMAL
     mov [rsp+0x30], dword 0
     
     call CreateFileA
 
+    FUNCTION_EPILOGUE 0x30
+
+
+
+_start:
+    mov rcx, my_filename
+    call open_file
+
+    mov rcx, my_other_filename
+    call open_file
+
     mov ecx, 0 
     call ExitProcess
-
-
-
-
